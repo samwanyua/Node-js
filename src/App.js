d208b39e717382627871b04508bd17f7398e7210
samwanyua <samexample8@gmail.com>
2024-07-17 01:10:28 +0300

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+// import './App.css';
+
+// JSX - Javascript and XML **
+
+function App() {
+  const name = "Samuel";
+
+  return (
+    <div className="App">
+      <h4>Welcome</h4>
+      <p>Hello, {name}</p>
+    </div>
+  );
+}
+
+export default App;
