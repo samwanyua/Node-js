c163ac0b22294a474a05299bcd3ab84e9452ccff
samwanyua <samexample8@gmail.com>
2024-07-17 12:18:29 +0300

    Write to a  file

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) =>
 
 console.log("Log data");
 
+fs.writeFile(path.join(__dirname, 'files',  'reply.txt'), 'Nice to meet you Sam', (err) => {
+    if (err) throw err;
+    console.log('Write complete')
+    // console.log(data.toString())
+
+})
+
 // exit on uncaught errors
 process.on('uncaughtException', err => {
     console.error(`There was an uncaught error: ${err}`);
