8a4b2f566364cc1a97e96a36e8460928308976ee
samwanyua <samexample8@gmail.com>
2024-07-17 12:14:39 +0300

    Read data from a file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.readFile('./files/starter.txt', 'utf8', (err, data) => {
+fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data)
     // console.log(data.toString())
