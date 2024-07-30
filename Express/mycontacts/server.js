2da48e13d7a65326b174b6a4c09598fd2496e4f7
samwanyua <samexample8@gmail.com>
2024-07-31 02:41:09 +0300

    Get all contacts

--- Express/mycontacts/server.js
+++ Express/mycontacts/server.js
@@ -6,7 +6,7 @@ const app = express(); // create an instance of Express application
 const port = process.env.PORT || 5000;
 
 app.get("/api/v1/contacts", (req, res) =>{
-    res.json({message: "Get all contacts"})
+    res.status(200).json({message: "Get all contacts"})
 })
 
 app.listen(port, ()=> {
