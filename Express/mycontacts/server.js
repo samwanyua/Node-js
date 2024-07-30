12386243bd26b27732ab76a32860b48e9693d12e
samwanyua <samexample8@gmail.com>
2024-07-31 02:19:15 +0300

    Initial configurations

--- Express/mycontacts/server.js
+++ Express/mycontacts/server.js
@@ -5,6 +5,10 @@ const app = express(); // create an instance of Express application
 
 const port = process.env.PORT || 5000;
 
+app.get("/api/v1/contacts", (req, res) =>{
+    res.json({message: "Get all contacts"})
+})
+
 app.listen(port, ()=> {
     console.log(`Server running on port ${port}`);
 })
