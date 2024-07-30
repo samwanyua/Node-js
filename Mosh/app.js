531e400b9817be71ffeb8a041bf735b4997d3245
samwanyua <samexample8@gmail.com>
2024-07-31 00:04:24 +0300

    file system module

--- Mosh/app.js
+++ Mosh/app.js
@@ -44,6 +44,7 @@ const fs = require('fs')
 const files = fs.readdirSync('./')
 console.log(files)
  
+// asynchronous method
 fs.readdir('./', function(err,files){
     if(err) console.log('Error', err)
     else console.log('Result', files)
