cdf33929936f89c904807ac3e2c7021e032cf895
samwanyua <samexample8@gmail.com>
2024-07-30 23:42:10 +0300

    Module wrapper function

--- Mosh/logger.js
+++ Mosh/logger.js
@@ -1,15 +1,15 @@
-// module wrapper function
-(funtion (exports,require, module, __filename, __dirname) {
-    var url = 'http://mylogger.io/log'
+console.log(__filename) // filename 
+console.log(__dirname) // directory path
 
-    function log(message){
-        // send a HTTP request
-        console.log(message);
-    }
+var url = 'http://mylogger.io/log'
 
-    // module.exports.log = log; // exporting log function
-    // exports.log = log; // alternative way
-    module.exports = log;
-    // module.exports.endPoint = url // exporting url variable
-})
+function log(message){
+    // send a HTTP request
+    console.log(message);
+}
+
+module.exports.log = log; // exporting log function
+// exports.log = log; // alternative way
+// module.exports = log;
+// module.exports.endPoint = url // exporting url variable
     
