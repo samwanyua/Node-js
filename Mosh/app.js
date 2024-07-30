b41a2f82c45b577b36770f449d90016fbe75f887
samwanyua <samexample8@gmail.com>
2024-07-30 23:57:46 +0300

    Os module

--- Mosh/app.js
+++ Mosh/app.js
@@ -36,7 +36,7 @@ console.log(pathObject)
 const os = require('os')
 const freeMem = os.freemem()
 const totalMemory= os.totalmem()
-console.log(freeMem)
+console.log(`Free memory: ${freeMem}`)
 console.log(`Total memory: ${totalMemory}`)
  
 
