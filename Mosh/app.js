bfe3783573db88e7bf639c01f7f93f1244182387
samwanyua <samexample8@gmail.com>
2024-07-30 23:06:54 +0300

    Global objects

--- Mosh/app.js
+++ Mosh/app.js
@@ -5,3 +5,10 @@ setInterval()
 clearInterval()
 
 window.console.log()
+
+var message = 'Jump'; // added to global scope, accessed via global object
+var sayHello = (name) {
+    console.log('Hello ' + name);
+}
+
+window.sayHello("Wanyua")
