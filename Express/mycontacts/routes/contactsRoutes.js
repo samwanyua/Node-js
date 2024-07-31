a6cd7b33101cbd35ad9078b77fc97209aab706eb
samwanyua <samexample8@gmail.com>
2024-07-31 23:51:54 +0300

    Set up controllers

--- Express/mycontacts/routes/contactsRoutes.js
+++ Express/mycontacts/routes/contactsRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const {getContact} = require('../controllers/contactController')
 
-router.route("/").get((req,res)=>{
-    res.status(200).json({message: "Get all contacts"});
-});
+router.route("/").get(getContact);
 
 router.route("/:id").get((req,res)=>{
     res.status(200).json({message: `Get contact for ${req.params.id}`});
