1c55445ed09d4655cc4ed7d5b163c2bf5c9e47fd
samwanyua <samexample8@gmail.com>
2024-08-01 16:20:07 +0300

    Update a single product

--- CRUD_API/index.js
+++ CRUD_API/index.js
@@ -45,6 +45,18 @@ app.post('/api/products', async (req, res) => {
 
 });
 
+// updating a product
+app.put('/api/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const updateProduct = await Product.findByIdAndUpdate(id, req.body);
+        
+        res.status(200).json(updateProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 // connecting to the database
 mongoose.connect("mongodb+srv://samexample8:S6w66d4vVyz0xStd@backend.nwoboto.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
 .then(()=> {
