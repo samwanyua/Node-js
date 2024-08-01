1229c577a91acdb1f7bf1cb6df667b2060cce1e9
samwanyua <samexample8@gmail.com>
2024-08-02 00:37:02 +0300

    Connecting to MongoDB database

--- Book_store/Backend/config.js
+++ Book_store/Backend/config.js
@@ -4,5 +4,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-export const PORT = 5555;
+export const PORT = process.env.PORT || 5555;
 export const MONGODB_URI = process.env.MONGODB_URI;
--- Book_store/Backend/index.js
+++ Book_store/Backend/index.js
@@ -1,28 +1,26 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import express from "express";
-import { PORT, MONGODB_URI } from "./config.js";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
+import { PORT, MONGODB_URI } from './config.js';
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    console.log(req);
-    return res.status(200).json({message: "Welcome to MERN stack!"})
-})
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(200).json({ message: "Welcome to MERN stack!" });
+});
 
-
-
-// connect to mongodb
+// Connect to MongoDB
 mongoose
-    .connect(MONGODB_URI)
-    .then(()=>{
-        console.log("App is connected to database");
-        app.listen(PORT, ()=>{
-            console.log(`App is listening to port: ${PORT}`)
-        })
-    })
-    .catch((error)=> {
-        console.log(error);
-    })
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("App is connected to the database");
+    app.listen(PORT, () => {
+      console.log(`App is listening on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
