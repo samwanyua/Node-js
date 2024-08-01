c5dc2e4924ceafbdf93ae1e97cfc394e52448192
samwanyua <samexample8@gmail.com>
2024-08-01 18:06:30 +0300

    Controllers

--- CRUD_API/routes/products.routes.js
+++ CRUD_API/routes/products.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('./models/product.model.js')
+const Product = require('../models/product.model.js')
 const {getProducts, getProduct, postProduct, updateProduct, deleteProduct} = require('../controllers/product.controller.js')
 
 router.get('/', getProducts );
