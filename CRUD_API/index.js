41516f0e4b351db94865ef3f9dad9db015c2f4dc
samwanyua <samexample8@gmail.com>
2024-08-01 15:17:54 +0300

    CRUD API with Node, Express and MongoDB

--- CRUD_API/index.js
+++ CRUD_API/index.js
@@ -13,3 +13,10 @@ app.get('/', (req, res)=> {
     res.send("Hello, from Node API ..");
 });
 
+mongoose.connect("mongodb+srv://samexample8:S6w66d4vVyz0xStd@backend.nwoboto.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
+.then(()=> {
+    console.log("Connected to the database");
+})
+.catch(() => {
+    console.log("Connection failed")
+})
