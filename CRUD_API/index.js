8560a39c79cbbd53c0271880a9c1e93b2a933abd
samwanyua <samexample8@gmail.com>
2024-08-01 16:40:25 +0300

    Delete a single product

--- CRUD_API/index.js
+++ CRUD_API/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 const mongoose = require('mongoose')
 const Product = require('./models/product.model.js')
 
