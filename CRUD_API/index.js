f4e853ad966df3e9a0714a622593a8f3f1ef1c83
samwanyua <samexample8@gmail.com>
2024-08-01 15:19:06 +0300

    CRUD API with Node, Express and MongoDB

--- CRUD_API/index.js
+++ CRUD_API/index.js
@@ -5,17 +5,19 @@ const mongoose = require('mongoose')
 
 const port = process.env.PORT || 5001
 
-app.listen(port,() =>{
-    console.log(`Server is running on ${port}`)
-});
 
 app.get('/', (req, res)=> {
     res.send("Hello, from Node API ..");
 });
 
+// connecting to the database
 mongoose.connect("mongodb+srv://samexample8:S6w66d4vVyz0xStd@backend.nwoboto.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
 .then(()=> {
     console.log("Connected to the database");
+    app.listen(port,() =>{
+        console.log(`Server is running on ${port}`)
+    });
+    
 })
 .catch(() => {
     console.log("Connection failed")
