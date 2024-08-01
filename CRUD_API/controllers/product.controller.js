92805d0e7cd366760f4410c37f45f5770903006e
samwanyua <samexample8@gmail.com>
2024-08-01 17:52:55 +0300

    Controllers

--- CRUD_API/controllers/product.controller.js
+++ CRUD_API/controllers/product.controller.js
@@ -10,6 +10,17 @@ const getProducts = async (req,res) => {
     }
 }
 
+const getProduct =  async (req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
 module.exports = {
     getProducts
 }
--- CRUD_API/routes/products.routes.js
+++ CRUD_API/routes/products.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('./models/product.model.js')
-const productController = require('../controllers/product.controller.js')
+const {getProducts} = require('../controllers/product.controller.js')
 
-router.get('/', )
+router.get('/', getProducts )
+router.get('/:id',getProduct)
+
+module.exports = router;
