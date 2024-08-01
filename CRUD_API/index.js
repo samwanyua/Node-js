d8fc0b908eac7e08d42c0ed2d28d10aeef1a44c6
samwanyua <samexample8@gmail.com>
2024-08-01 14:55:20 +0300

    CRUD API with Node, Express and MongoDB

--- CRUD_API/index.js
+++ CRUD_API/index.js
@@ -5,4 +5,8 @@ const port = process.env.PORT || 5001
 
 app.listen(port,() =>{
     console.log(`Server is running on ${port}`)
+});
+
+app.get('/', (req, res)=> {
+    res.send("Hello, from Node API");
 })
