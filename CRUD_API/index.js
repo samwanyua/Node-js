f22f2d51f79982db31860c3d190b7cc13daf8d8d
samwanyua <samexample8@gmail.com>
2024-08-01 16:29:10 +0300

    Delete a single product

--- CRUD_API/index.js
+++ CRUD_API/index.js
@@ -60,6 +60,23 @@ app.put('/api/products/:id', async(req, res) => {
     } catch (error) {
         res.status(500).json({message: error.message})
     }
+});
+
+// delete a product
+app.delete('/api/products/:id', async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if(!product){
+            return res.status(404).json({message: "Product not found"})
+        }
+
+        res.status(200).json({message: "Product deleted successfully"});
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 })
 
 // connecting to the database
